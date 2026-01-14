5789d993097afd8ad5b12d8213a001b5f23d9fe0
bogumil <bogumil_latuszek@o2.pl>
2026-01-14 22:24:36 +0100

    changed OnGet() to return NotFound() if requested imageId doesn't exist in db (in ImageService for now)
    I'm not exactly sure why OnGet can be changed from void to return type IActionResult without breaking the program, There might be more to ASP.Net then I thought

--- Pages/ItemDetails.cshtml.cs
+++ Pages/ItemDetails.cshtml.cs
@@ -1,5 +1,6 @@
 using imageboard.Models;
 using imageboard.Services;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace imageboard.Pages;
@@ -9,9 +10,21 @@ public class ItemDetails : PageModel
 {
     public Image? item;
     
-    public void OnGet(int imageId)
+    public IActionResult OnGet(int imageId)
     {
         ImageService service = new ImageService();
         item = service.GetImage(imageId);
+        if (item == null)
+        {
+            return NotFound();  // Returns 404 status code
+        }
+
+        return Page();
     }
+    
+    // public void OnGet(int imageId)
+    // {
+    //     ImageService service = new ImageService();
+    //     item = service.GetImage(imageId);
+    // }
 }
